36e66ca67a3a5409f5db2d3209fd9c7df633a490
yotam-biu <nagaryo3@biu.ac.il>
2024-12-28 14:26:08 +0200

    Update model_test.py

--- .tests/model_test.py
+++ .tests/model_test.py
@@ -1,5 +1,6 @@
 import joblib
+from main import main
 
 def test_model():
-    #model = joblib.load("penguins_model.joblib")
-    assert True
+    #model = joblib.load("penguins_model.joblib"
+    assert main() == 222
