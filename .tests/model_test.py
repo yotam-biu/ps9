b27bd11ae3a28e8b27a1af68791b611c28196f97
yotam-biu <nagaryo3@biu.ac.il>
2024-12-28 15:25:32 +0200

    Update model_test.py

--- .tests/model_test.py
+++ .tests/model_test.py
@@ -3,12 +3,13 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 
 def test_model():
-    penguins_test = pd.read_csv("penguins_test.csv")
+    penguins_test = pd.read_csv(".tests/penguins_test.csv")
     penguins_test = penguins_test.dropna() # remove none
     X = penguins_test.drop(columns = ["species"])
     y = penguins_test["species"] # categorial
     features = ['bill_length_mm', 'bill_depth_mm']
     X = X[features]
+    model = joblib.load("penguins_model.joblib")
     prediction = model.predict(X)
     score = accuracy_score(y, prediction)
     assert score > 0.95
