ad0ba3aa2535b60e3221cb69a85bdb2e776d576f
yotam-biu <nagaryo3@biu.ac.il>
2024-12-28 15:08:55 +0200

    Update model_test.py

--- .tests/model_test.py
+++ .tests/model_test.py
@@ -3,8 +3,8 @@ from main import main
 import pandas as pd
 
 def test_model():
-    # data_path = ".tests/penguins_test.csv"
-    # data_test = pd.read_csv(data_path)
-    mode_path = "penguins_model.joblib"
+    data_path = ".tests/penguins_test.csv"
+    data_test = pd.read_csv(data_path)
+    model_path = "penguins_model.joblib"
     model = joblib.load(model_path)
     assert main() == 274
