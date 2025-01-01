5fad48c74b47091f1a4cecea43d6cc9a9352f988
yotam-biu <nagaryo3@biu.ac.il>
2025-01-01 09:10:48 +0200

    Update model_test.py

--- .tests/model_test.py
+++ .tests/model_test.py
@@ -6,21 +6,21 @@ import yaml
 def test_model():
 
 
-    # # Load the YAML file
-    # with open('config.yaml', 'r') as file:
-    #     config = yaml.safe_load(file)
+    # Load the YAML file
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
     
-    # # Access the data
-    # path = config['path']
-    # features = config['features']
-    # assert len(features) == 2
+    # Access the data
+    path = config['path']
+    features = config['features']
+    assert len(features) == 2
 
 
     
     df_test = pd.read_csv(".tests/test.data")
-    X = df_test[['PPE', 'DFA']]
+    X = df_test[features]
     y = df_test["status"] # categorial
-    model = joblib.load("svc_parkinsons.joblib")
+    model = joblib.load(path)
     prediction = model.predict(X)
     score = accuracy_score(y, prediction)
     assert score > 0.7
